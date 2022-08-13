549c725987023f6f183a9d3cc31c4bebc86fe8ee
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-08-13 13:09:27 +0900

    firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'ui/topPage.dart';
 import 'diagnose/topPage.dart';
 import 'company_priority/topPage.dart';
 import 'diagnose/answer.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
